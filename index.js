d2cb79ebbf49673ca5d5e98d84a8fd6a4ecf2d81
AsadovIbrahim <ibrahimasadov31@gmail.com>
2024-11-05 18:04:15 +0400

    commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const { authenticateToken, authorizeUser } = require('./middleware/authMiddleware');
 const { connectToDb } = require("./utils/databaseUtils")
 require('dotenv').config();
 const userRoutes = require('./routes/userRoutes');
@@ -13,9 +14,12 @@ connectToDb()
 app.use("/todos", todoRoutes)
 app.use("/users", userRoutes)
 app.use("/auth", authRoutes)
+app.use(authenticateToken)
+app.use(authorizeUser);
 
 const PORT = process.env.PORT
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
--- utils/databaseUtils.js
+++ utils/databaseUtils.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose")
 
 const connectToDb = () =>{
     try {
-        mongoose.connect(process.env.DB_URL)
+        mongoose.connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log('Connected to Mongo database');
     } catch (error) {
         console.log("Error while connecting to DB:", error)
